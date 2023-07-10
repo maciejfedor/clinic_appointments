86ab00f9f7226a36533d73aed27f4b73745448a9
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-10 17:15:36 +0200

    add csv patients import

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,21 +5,34 @@
 # Table name: patients
 #
 #  id            :uuid             not null, primary key
+#  city          :string           not null
 #  date_of_birth :date             not null
 #  first_name    :string           not null
 #  gender        :string           not null
 #  last_name     :string           not null
+#  uid           :string           not null
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_patients_on_uid  (uid) UNIQUE
 #
 class Patient < ApplicationRecord
   has_person_name
 
+  # Validations
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :gender, presence: true
   validates :date_of_birth, presence: true
+  validates :city, presence: true
 
+  # uid is a unique identifier for a patient in the system
+  # it is a PESEL number for polish patients
+  validates :uid, presence: true, uniqueness: true
+
+  # Enums
   enum gender: {
     male: "Male",
     female: "Female",
---
+++ lib/data_import/import_patients.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "csv"
+
+module DataImport
+  class ImportPatients < Base
+    FILE_PATH = "#{Rails.root}/db/data/patients_import/fake_medical_patients.csv".freeze
+    BATCH_SIZE = 1000 # Adjust the batch size as needed
+
+    def initialize
+      @source_file = CSV.open(FILE_PATH, headers: true, header_converters: :symbol)
+    end
+
+    def call
+      raise StandardError, "File not found" unless @source_file
+
+      log_info "PATIENTS IMPORT STARTED..."
+
+      @source_file.each_slice(BATCH_SIZE) do |batch|
+        patient_records = build_patient_records(batch)
+        create_patients(patient_records)
+      end
+      log_info "PATIENTS IMPORT FINISHED!"
+    end
+
+    private
+
+    def build_patient_records(records)
+      records.map do |patient|
+        {
+          uid: patient[:pesel],
+          first_name: patient[:first_name],
+          last_name: patient[:last_name],
+          gender: gender(patient[:pesel]),
+          date_of_birth: date_of_birth(patient[:pesel]),
+          city: patient[:city],
+        }
+      end
+    end
+
+    def create_patients(records)
+      Patient.upsert_all(records, unique_by: :uid)
+    end
+
+    def date_of_birth(pesel)
+      year = pesel[0..1]
+      month = pesel[2..3]
+      day = pesel[4..5]
+
+      if month.to_i > 12
+        month = month.to_i - 20
+        year = "20#{year}"
+      else
+        year = "19#{year}"
+      end
+      Date.new(year.to_i, month.to_i, day.to_i)
+    end
+
+    def gender(pesel)
+      gender_number = pesel[9].to_i
+      gender_number.even? ? "Female" : "Male"
+    end
+  end
+end
