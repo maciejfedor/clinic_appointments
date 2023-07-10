10c54a079c995f12a0097eaa5464e2f1e9271107
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-10 17:10:00 +0200

    add uid and city to patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_183610) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_114734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_183610) do
     t.date "date_of_birth", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city", null: false
+    t.string "uid", null: false
+    t.index ["uid"], name: "index_patients_on_uid", unique: true
   end
 
 end
