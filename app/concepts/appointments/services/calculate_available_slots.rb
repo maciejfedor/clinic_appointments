362e266c78e31245f18eea649f89ecc5da9ee016
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-15 16:47:59 +0200

    add available slots service and controller

---
+++ app/concepts/appointments/services/calculate_available_slots.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Appointments
+  module Services
+    class CalculateAvailableSlots < ApplicationService
+      APPOINTMENT_DURATION = 20
+      WORKING_HOURS = {
+        start: 8,
+        end: 16,
+      }.freeze
+
+      def initialize(doctor_id:, appointment_date:)
+        @doctor_id = doctor_id
+        @appointment_date = appointment_date.to_datetime
+        @doctor_appointments = preload_appointments
+      end
+
+      def call
+        time_slots = []
+        (WORKING_HOURS[:start]...WORKING_HOURS[:end]).each do |hour|
+          0.upto(slots_per_hour - 1) do |minute|
+            slot = DateTime.new(@appointment_date.year, @appointment_date.month, @appointment_date.day, hour,
+                                minute * APPOINTMENT_DURATION)
+            time_slots << slot.strftime("%H:%M") unless taken?(slot)
+          end
+        end
+        time_slots
+      end
+
+      private
+
+      def slots_per_hour
+        (60 / APPOINTMENT_DURATION)
+      end
+
+      def preload_appointments
+        doctor.appointments
+              .where(start_time: @appointment_date.beginning_of_day..@appointment_date.end_of_day)
+              .pluck(:start_time)
+      end
+
+      def taken?(slot)
+        slot.after?(Time.zone.now) && @doctor_appointments.include?(slot)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  get "/appointments/available_slots", to: "available_slots#show"
   root "patients#index"
 
   resources :patients, only: [:index] do
