2d940c9b6b3f46e33978ecd0b6b8193bc68b9509
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-15 19:50:53 +0200

    fix flash display

--- test/components/previews/flash_component_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class FlashComponentPreview < Lookbook::Preview
-  def success
-    render FlashComponent.new(level: "success", text: "Succesfully created!")
-  end
-end
