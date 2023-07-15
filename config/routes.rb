a1c555c634c61cab4e79948f6c07e9fd71fdfa0b
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-15 19:51:50 +0200

    add lookbok route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Lookbook::Engine, at: "/lookbook" if Rails.env.development?
   get "/appointments/available_slots", to: "available_slots#show"
   root "patients#index"
 
