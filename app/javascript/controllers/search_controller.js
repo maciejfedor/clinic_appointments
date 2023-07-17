6490619e416761f3ff195190e1b3351f1ba1227d
Maciej Fedor <fedor.maciej@gmail.com>
2023-07-17 13:17:54 +0200

    remove debounce from search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,9 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
-import debounce from "https://cdn.jsdelivr.net/npm/debounce@1.2.1/+esm";
 
 export default class extends Controller {
   connect() {
-    this.submit = debounce(this.submit.bind(this), 200);
+    this.submit = this.submit.bind(this);
   }
 
   submit(_event) {
